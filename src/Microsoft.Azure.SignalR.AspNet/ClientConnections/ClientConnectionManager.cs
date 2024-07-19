bfa999315dddbb0f1166d8f8dec8a305321d3ffe
Terence Fan <stdrickforce@gmail.com>
2024-07-19 13:15:32 +0800

    convert to file-scoped namespace

--- src/Microsoft.Azure.SignalR.AspNet/ClientConnections/ClientConnectionManager.cs
+++ src/Microsoft.Azure.SignalR.AspNet/ClientConnections/ClientConnectionManager.cs
@@ -14,140 +14,139 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Owin;
 
-namespace Microsoft.Azure.SignalR.AspNet
+namespace Microsoft.Azure.SignalR.AspNet;
+
+internal class ClientConnectionManager : IClientConnectionManager
 {
-    internal class ClientConnectionManager : IClientConnectionManager
+    private readonly HubConfiguration _configuration;
+    private readonly ILogger _logger;
+
+    private readonly ConcurrentDictionary<string, ClientConnectionContext> _clientConnections = new ConcurrentDictionary<string, ClientConnectionContext>();
+
+    public ClientConnectionManager(HubConfiguration configuration, ILoggerFactory loggerFactory)
     {
-        private readonly HubConfiguration _configuration;
-        private readonly ILogger _logger;
+        _configuration = configuration;
+        _logger = loggerFactory?.CreateLogger<ClientConnectionManager>() ?? NullLogger<ClientConnectionManager>.Instance;
+    }
 
-        private readonly ConcurrentDictionary<string, ClientConnectionContext> _clientConnections = new ConcurrentDictionary<string, ClientConnectionContext>();
+    public async Task<IServiceTransport> CreateConnection(OpenConnectionMessage message)
+    {
+        var dispatcher = new ClientConnectionHubDispatcher(_configuration, message.ConnectionId);
+        dispatcher.Initialize(_configuration.Resolver);
 
-        public ClientConnectionManager(HubConfiguration configuration, ILoggerFactory loggerFactory)
+        var responseStream = new MemoryStream();
+        var hostContext = GetHostContext(message, responseStream);
+
+        if (dispatcher.Authorize(hostContext.Request))
         {
-            _configuration = configuration;
-            _logger = loggerFactory?.CreateLogger<ClientConnectionManager>() ?? NullLogger<ClientConnectionManager>.Instance;
-        }
+            // ProcessRequest checks if the connectionToken matches "{connectionid}:{userName}" format with context.User
+            await dispatcher.ProcessRequest(hostContext);
 
-        public async Task<IServiceTransport> CreateConnection(OpenConnectionMessage message)
-        {
-            var dispatcher = new ClientConnectionHubDispatcher(_configuration, message.ConnectionId);
-            dispatcher.Initialize(_configuration.Resolver);
-
-            var responseStream = new MemoryStream();
-            var hostContext = GetHostContext(message, responseStream);
-
-            if (dispatcher.Authorize(hostContext.Request))
+            // TODO: check for errors written to the response
+            if (hostContext.Response.StatusCode != 200)
             {
-                // ProcessRequest checks if the connectionToken matches "{connectionid}:{userName}" format with context.User
-                await dispatcher.ProcessRequest(hostContext);
-
-                // TODO: check for errors written to the response
-                if (hostContext.Response.StatusCode != 200)
-                {
-                    Log.ProcessRequestError(_logger, message.ConnectionId, hostContext.Request.QueryString.ToString());
-                    var errorResponse = GetContentAndDispose(responseStream);
-                    throw new InvalidOperationException(errorResponse);
-                }
-
-                return (AzureTransport)hostContext.Environment[AspNetConstants.Context.AzureSignalRTransportKey];
+                Log.ProcessRequestError(_logger, message.ConnectionId, hostContext.Request.QueryString.ToString());
+                var errorResponse = GetContentAndDispose(responseStream);
+                throw new InvalidOperationException(errorResponse);
             }
 
-            // This happens when hub is not found
-            throw new InvalidOperationException("Unable to authorize request");
+            return (AzureTransport)hostContext.Environment[AspNetConstants.Context.AzureSignalRTransportKey];
         }
 
-        public bool TryAddClientConnection(ClientConnectionContext connection)
+        // This happens when hub is not found
+        throw new InvalidOperationException("Unable to authorize request");
+    }
+
+    public bool TryAddClientConnection(ClientConnectionContext connection)
+    {
+        return _clientConnections.TryAdd(connection.ConnectionId, connection);
+    }
+
+    public bool TryRemoveClientConnection(string connectionId, out ClientConnectionContext connection)
+    {
+        return _clientConnections.TryRemove(connectionId, out connection);
+    }
+
+    public bool TryGetClientConnection(string connectionId, out ClientConnectionContext connection)
+    {
+        return _clientConnections.TryGetValue(connectionId, out connection);
+    }
+
+    public IReadOnlyDictionary<string, ClientConnectionContext> ClientConnections => _clientConnections;
+
+    internal HostContext GetHostContext(OpenConnectionMessage message, Stream responseStream)
+    {
+        var connectionId = message.ConnectionId;
+        var context = new OwinContext();
+        var response = context.Response;
+        var request = context.Request;
+
+        response.Body = responseStream;
+
+        var user = request.User = message.GetUserPrincipal();
+
+        request.Path = new PathString("/");
+
+        string queryString = message.QueryString;
+        if (queryString.Length > 0)
         {
-            return _clientConnections.TryAdd(connection.ConnectionId, connection);
-        }
-
-        public bool TryRemoveClientConnection(string connectionId, out ClientConnectionContext connection)
-        {
-            return _clientConnections.TryRemove(connectionId, out connection);
-        }
-
-        public bool TryGetClientConnection(string connectionId, out ClientConnectionContext connection)
-        {
-            return _clientConnections.TryGetValue(connectionId, out connection);
-        }
-
-        public IReadOnlyDictionary<string, ClientConnectionContext> ClientConnections => _clientConnections;
-
-        internal HostContext GetHostContext(OpenConnectionMessage message, Stream responseStream)
-        {
-            var connectionId = message.ConnectionId;
-            var context = new OwinContext();
-            var response = context.Response;
-            var request = context.Request;
-
-            response.Body = responseStream;
-
-            var user = request.User = message.GetUserPrincipal();
-
-            request.Path = new PathString("/");
-
-            string queryString = message.QueryString;
-            if (queryString.Length > 0)
+            // The one from Azure SignalR always contains a leading '?' character however the Owin one does not
+            if (queryString[0] == '?')
             {
-                // The one from Azure SignalR always contains a leading '?' character however the Owin one does not
-                if (queryString[0] == '?')
-                {
-                    queryString = queryString.Substring(1);
-                }
-
-                request.QueryString = new QueryString(queryString);
+                queryString = queryString.Substring(1);
             }
 
-            if (message.Headers != null)
-            {
-                foreach (var pair in message.Headers)
-                {
-                    request.Headers.Add(pair.Key, pair.Value);
-                }
-            }
-            return new HostContext(context.Environment);
+            request.QueryString = new QueryString(queryString);
         }
 
-        internal static string GetContentAndDispose(MemoryStream stream)
+        if (message.Headers != null)
         {
-            stream.Seek(0, SeekOrigin.Begin);
-            using (var reader = new StreamReader(stream))
+            foreach (var pair in message.Headers)
             {
-                return reader.ReadToEnd();
+                request.Headers.Add(pair.Key, pair.Value);
             }
         }
+        return new HostContext(context.Environment);
+    }
 
-        public Task WhenAllCompleted() => Task.CompletedTask;
-
-
-        private sealed class ClientConnectionHubDispatcher : HubDispatcher
+    internal static string GetContentAndDispose(MemoryStream stream)
+    {
+        stream.Seek(0, SeekOrigin.Begin);
+        using (var reader = new StreamReader(stream))
         {
-            private readonly string _connectionId;
+            return reader.ReadToEnd();
+        }
+    }
 
-            public ClientConnectionHubDispatcher(HubConfiguration config, string connectionId) : base(config)
-            {
-                _connectionId = connectionId;
-            }
+    public Task WhenAllCompleted() => Task.CompletedTask;
 
-            protected override bool TryGetConnectionId(HostContext context, string connectionToken, out string connectionId, out string message, out int statusCode)
-            {
-                connectionId = _connectionId;
-                message = null;
-                statusCode = 200;
-                return true;
-            }
+
+    private sealed class ClientConnectionHubDispatcher : HubDispatcher
+    {
+        private readonly string _connectionId;
+
+        public ClientConnectionHubDispatcher(HubConfiguration config, string connectionId) : base(config)
+        {
+            _connectionId = connectionId;
         }
 
-        private static class Log
+        protected override bool TryGetConnectionId(HostContext context, string connectionToken, out string connectionId, out string message, out int statusCode)
         {
-            private static readonly Action<ILogger, string, string, Exception> _processRequestError =
-                LoggerMessage.Define<string, string>(LogLevel.Debug, new EventId(1, "ProcessRequestError"), "ProcessRequest for {connectionId} fails with {queryString} ");
+            connectionId = _connectionId;
+            message = null;
+            statusCode = 200;
+            return true;
+        }
+    }
 
-            public static void ProcessRequestError(ILogger logger, string connectionId, string queryString)
-            {
-                _processRequestError(logger, connectionId, queryString, null);
-            }
+    private static class Log
+    {
+        private static readonly Action<ILogger, string, string, Exception> _processRequestError =
+            LoggerMessage.Define<string, string>(LogLevel.Debug, new EventId(1, "ProcessRequestError"), "ProcessRequest for {connectionId} fails with {queryString} ");
+
+        public static void ProcessRequestError(ILogger logger, string connectionId, string queryString)
+        {
+            _processRequestError(logger, connectionId, queryString, null);
         }
     }
 }
